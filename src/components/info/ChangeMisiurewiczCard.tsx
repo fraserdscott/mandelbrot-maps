098da9eac88625f883ed2224206bcb055de60a66
fraserdscott <fraserdscott@gmail.com>
2020-10-20 21:30:55 +0100

    Add a Snackbar for the reset state

--- src/components/info/ChangeMisiurewiczCard.tsx
+++ src/components/info/ChangeMisiurewiczCard.tsx
@@ -5,13 +5,17 @@ import { misiurewiczPoints } from '../../App';
 import { warpToPoint } from '../utils';
 
 const ChangeMisiurewiczCard = (props: ChangeMisiurewiczCardProps): JSX.Element => {
-  const [openT, setOpenT] = React.useState(false);
-  const [openM, setOpenM] = React.useState(false);
-  const [openR, setOpenR] = React.useState(false);
+  const [animationState, setAnimationState] = React.useState(0);
+  const [pointToAnimate, setPointToAnimate] = React.useState([0, 0]);
 
-  const [messageT, setMessageT] = React.useState('not set');
-  const [messageM, setMessageM] = React.useState('not set');
-  const [messageR, setMessageR] = React.useState('not set');
+  const [showReset, setOpenReset] = React.useState(false);
+  const [showTranslation, setOpenT] = React.useState(false);
+  const [showMagnification, setOpenM] = React.useState(false);
+  const [showRotation, setOpenR] = React.useState(false);
+
+  const [messageTranslation, setMessageT] = React.useState('not set');
+  const [messageMagnification, setMessageM] = React.useState('not set');
+  const [messageRotation, setMessageR] = React.useState('not set');
 
   const handleClose = (
     event: React.SyntheticEvent | React.MouseEvent,
@@ -21,39 +25,40 @@ const ChangeMisiurewiczCard = (props: ChangeMisiurewiczCardProps): JSX.Element =
       return;
     }
 
+    setOpenReset(false);
     setOpenT(false);
     setOpenM(false);
     setOpenR(false);
   };
 
-  const [state, setState] = React.useState(0);
-  const [pointToAnimate, setPointToAnimate] = React.useState([0, 0]);
   const go = (pos: [number, number], zoom: number, theta: number) => {
+    // If the user has clicked on a different point to the one we are currently animating
     if (pos[0] !== pointToAnimate[0] && pos[1] !== pointToAnimate[1]) {
-      setState(0);
+      setAnimationState(0);
       setPointToAnimate(pos);
       setMessageT(`Translating to ${pos}`);
       setMessageM(`Magnifying ${zoom}x`);
       setMessageR(`Rotating by ${theta} radians`);
-    } else if (state === 0) {
-      warpToPoint(props.mandelbrot, { xy: [0, 0], z: 0.8, theta: 0 });
-      warpToPoint(props.julia, { xy: [0, 0], z: 0.8, theta: 0 });
-      setState(1);
-    } else if (state === 1) {
+    } else if (animationState === 0) {
+      setOpenReset(true);
+      warpToPoint(props.mandelbrot, { xy: [-0.45, 0], z: 0.8, theta: 0 });
+      warpToPoint(props.julia, { xy: [-0.45, 0], z: 0.8, theta: 0 });
+      setAnimationState(1);
+    } else if (animationState === 1) {
       setOpenT(true);
       warpToPoint(props.mandelbrot, { xy: pos, z: 1, theta: 0 });
       warpToPoint(props.julia, { xy: pos, z: 1, theta: 0 });
-      setState(2);
-    } else if (state === 2) {
+      setAnimationState(2);
+    } else if (animationState === 2) {
       setOpenM(true);
       warpToPoint(props.mandelbrot, { xy: pos, z: zoom, theta: 0 });
       warpToPoint(props.julia, { xy: pos, z: zoom, theta: 0 });
-      setState(3);
-    } else if (state === 3) {
+      setAnimationState(3);
+    } else if (animationState === 3) {
       setOpenR(true);
       warpToPoint(props.mandelbrot, { xy: pos, z: zoom, theta: theta });
       warpToPoint(props.julia, { xy: pos, z: zoom, theta: 0 });
-      setState(4);
+      setAnimationState(4);
     }
   };
 
@@ -88,30 +93,40 @@ const ChangeMisiurewiczCard = (props: ChangeMisiurewiczCardProps): JSX.Element =
               vertical: 'top',
               horizontal: 'center',
             }}
-            open={openT}
+            open={showReset}
             onClose={handleClose}
             autoHideDuration={3000}
-            message={messageT}
+            message={'Resetting'}
           />
           <Snackbar
             anchorOrigin={{
               vertical: 'top',
               horizontal: 'center',
             }}
-            open={openM}
+            open={showTranslation}
             onClose={handleClose}
             autoHideDuration={3000}
-            message={messageM}
+            message={messageTranslation}
           />
           <Snackbar
             anchorOrigin={{
               vertical: 'top',
               horizontal: 'center',
             }}
-            open={openR}
+            open={showMagnification}
             onClose={handleClose}
             autoHideDuration={3000}
-            message={messageR}
+            message={messageMagnification}
+          />
+          <Snackbar
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+            open={showRotation}
+            onClose={handleClose}
+            autoHideDuration={3000}
+            message={messageRotation}
           />
         </Grid>
       </Card>
