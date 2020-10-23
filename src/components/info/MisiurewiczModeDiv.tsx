d6332ac07af80f95d5beac88daef98f3700130cf
fraserdscott <fraserdscott@gmail.com>
2020-10-23 19:07:28 +0100

    Forgot to commit new files for markers!

---
+++ src/components/info/MisiurewiczModeDiv.tsx
@@ -0,0 +1,55 @@
+import React, { Dispatch, SetStateAction } from 'react';
+import { MisiurewiczModeDivProps } from '../../common/info';
+import SelectMisiurewiczCard from './SelectMisiurewiczCard';
+import { misiurewiczPoints } from './SelectMisiurewiczCard';
+import MisiurewiczPointMarker from './MisiurewiczPointMarker';
+
+const MisiurewiczModeDiv = (props: MisiurewiczModeDivProps): JSX.Element => {
+  const [animationState, setAnimationState] = React.useState(0);
+  const [mag, setMagState] = React.useState<number>(1);
+  const [focusedPoint, setFocusedPoint]: [
+    [number, number],
+    Dispatch<SetStateAction<[number, number]>>,
+  ] = React.useState(misiurewiczPoints[0]);
+
+  return (
+    <>
+      {misiurewiczPoints.map((m) => (
+        <MisiurewiczPointMarker
+          m={m}
+          show={animationState === 0}
+          mandelbrot={props.mandelbrot}
+          julia={props.julia}
+          animationState={animationState}
+          setAnimationState={setAnimationState}
+          focusedPoint={focusedPoint}
+          setFocusedPoint={setFocusedPoint}
+          mag={mag}
+          setMagState={setMagState}
+        />
+      ))}
+      <div
+        style={{
+          position: 'absolute',
+          left: 0,
+          top: 0,
+          width: 'auto',
+        }}
+      >
+        <SelectMisiurewiczCard
+          show={props.show}
+          mandelbrot={props.mandelbrot}
+          julia={props.julia}
+          animationState={animationState}
+          setAnimationState={setAnimationState}
+          focusedPoint={focusedPoint}
+          setFocusedPoint={setFocusedPoint}
+          mag={mag}
+          setMagState={setMagState}
+        />
+      </div>
+    </>
+  );
+};
+
+export default MisiurewiczModeDiv;
---
+++ src/components/info/MisiurewiczPointMarker.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import { Grow } from '@material-ui/core';
+import RoomIcon from '@material-ui/icons/Room';
+import IconButton from '@material-ui/core/IconButton';
+import { screenScaleMultiplier } from '../../common/values';
+import { MisiurewiczPointMarkerProps } from '../../common/info';
+import { animated } from 'react-spring';
+import { warpToPoint } from '../utils';
+import { magnificationMandelbrot, magnificationJulia } from '../tansTheoremUtils';
+
+const MisiurewiczPointMarker = (props: MisiurewiczPointMarkerProps): JSX.Element => {
+  const [{ z }, setControlZoom] = props.mandelbrot.zoomCtrl;
+  const width = 640;
+  const height = 610;
+
+  const handlePointSelection = (chosenPoint: [number, number]) => {
+    if (
+      chosenPoint[0] !== props.focusedPoint[0] &&
+      chosenPoint[1] !== props.focusedPoint[1]
+    ) {
+      props.setAnimationState(0);
+      props.setFocusedPoint(chosenPoint);
+      props.setMagState(1);
+
+      const zoomM: number = magnificationMandelbrot(props.focusedPoint) * 1;
+      const zoomJ: number = magnificationJulia(props.focusedPoint) * 1;
+
+      warpToPoint(props.mandelbrot, { xy: chosenPoint, z: zoomM, theta: 0 });
+      warpToPoint(props.julia, { xy: chosenPoint, z: zoomJ, theta: 0 });
+    }
+  };
+
+  return (
+    <Grow in={props.show}>
+      <animated.div
+        style={{
+          position: 'absolute',
+          left: props.mandelbrot.xyCtrl[0].xy.interpolate(
+            // @ts-expect-error: Function call broken in TS, waiting till react-spring v9 to fix
+            (x, y) =>
+              width / 2 +
+              (width / 2) * (props.m[0] - x * screenScaleMultiplier) * z.getValue() -
+              3 * (40 / 4),
+          ),
+          bottom: props.mandelbrot.xyCtrl[0].xy.interpolate(
+            // @ts-expect-error: Function call broken in TS, waiting till react-spring v9 to fix
+            (x, y) =>
+              height / 2 +
+              (height / 2) * (props.m[1] - y * screenScaleMultiplier) * z.getValue() -
+              40 / 4,
+          ),
+          width: 'auto',
+        }}
+      >
+        <div
+          style={{
+            width: 'auto',
+            zIndex: 100,
+            position: 'relative',
+          }}
+        >
+          <IconButton
+            onClick={() => {
+              handlePointSelection(props.m);
+            }}
+            color="secondary"
+          >
+            <RoomIcon style={{ width: 40, height: 40 }} />
+          </IconButton>
+        </div>
+      </animated.div>
+    </Grow>
+  );
+};
+
+export default MisiurewiczPointMarker;
